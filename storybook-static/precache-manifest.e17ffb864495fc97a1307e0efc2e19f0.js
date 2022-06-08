bb15e4e2366f18422b69340c80c5268e7e8def5d
GmBarcha09 <itsgm.barcha09>
2022-06-09 03:35:00 +0500

    adding changes

---
+++ storybook-static/precache-manifest.e17ffb864495fc97a1307e0efc2e19f0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8a05f6415ae7d95decf0f0bfa5d4007",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.923c889a66cf0b7bb52f.bundle.js"
+  },
+  {
+    "url": "runtime~main.923c889a66cf0b7bb52f.bundle.js"
+  },
+  {
+    "revision": "c584465fd17282987d7d",
+    "url": "static/css/main.ebb57f73.chunk.css"
+  },
+  {
+    "url": "vendors~main.923c889a66cf0b7bb52f.bundle.js"
+  },
+  {
+    "url": "vendors~main.923c889a66cf0b7bb52f.bundle.js.LICENSE.txt"
+  }
+]);
